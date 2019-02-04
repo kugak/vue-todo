4ce3b5fc99961b0806cfad334528996eaf90cb35
Kugakanth Kugarajasingam <kugarajk@sheridanc.on.ca>
2019-02-04 10:47:53 -0500

    added todos.vue and props

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Hello World!"/>
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
+import Todos from "./components/Todos";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    };
+  }
 };
 </script>
 
